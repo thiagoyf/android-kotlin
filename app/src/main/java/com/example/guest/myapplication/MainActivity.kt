81dadebc4def04302c198be8a0b10fde01ee9237
Guest User <Guest@Mac-mini-de-Tadashi.local>
2019-06-03 13:36:12 -0400

    Application Kotlin

--- app/src/main/java/com/example/guest/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/guest/myapplication/MainActivity.kt
@@ -1,12 +1,53 @@
 package com.example.guest.myapplication
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.TextView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun toastMe(view: View) {
+        val myToast = Toast.makeText(this, "Hello Toast!", Toast.LENGTH_LONG)
+        myToast.show()
+    }
+
+    fun countMe(view: View) {
+        // Get the text view
+        val showCountTextView = findViewById<TextView>(R.id.textView)
+
+        // Get the value of the text view
+        val countString = showCountTextView.text.toString()
+
+        // Convert value to a number and increment it
+        var count: Int = Integer.parseInt(countString)
+        count++
+
+        showCountTextView.text = count.toString()
+    }
+
+    fun randomMe(view: View) {
+        // Create an Intent to start the second activity
+        val randomIntent = Intent(this, SecondActivity::class.java)
+
+        val showCountTextView = findViewById<TextView>(R.id.textView)
+
+        // Get the value of the text view
+        val countString = showCountTextView.text.toString()
+
+        // Convert the count to an int
+        val count = Integer.parseInt(countString)
+
+        randomIntent.putExtra(SecondActivity.TOTAL_COUNT, count)
+
+        // Start the new activity.
+        startActivity(randomIntent)
     }
 }
